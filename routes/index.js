54a2aafdc2d4f14258cec6aae3cab7fcb3cee350
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-17 11:20:44 -0700

    added persons array to the view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//sample messages
+const messages = [
+  {
+    text:"Hi Baltej",
+    user:"layla",
+    added: new Date(),
+  },
+  {
+    text:"Hi there big titty",
+    user: "baltej",
+    added: new Date(),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title:'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
